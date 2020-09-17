b50e9233aef02d4de6d2092a4af70294046b64bc
Will-Harris00 <wjharris00@outlook.com>
2020-09-17 17:07:59 +0100

    Choice of Vowel or Consonant

--- Random Character Selector.py
+++ Random Character Selector.py
@@ -1,22 +1,36 @@
-from random import sample, randint
-from random import shuffle
+"""Imports the python function for random number generator"""
+import random
 
 
-def generate_word():
-    vowels = 'eeeeeeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaiiiiiiiiiiiiiooooooooooooouuuuu'
-    consonants = 'bbcccddddddffggghhjklllllmmmmnnnnnnnnppppqrrrrrrrrrssssssssstttttttttvwxyz'
-    n_vowels = randint(3, 5)
-    vowels = sample(vowels, n_vowels)
-    consonants = sample(consonants, 9 - n_vowels)
-    letters = vowels + consonants
-    print(letters)
-    shuffle(letters)
-    word = ''.join(letters)
-    print(word)
+def select_characters():
+    """Defines the function used to select the letters used in the game"""
+    letters = ""
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+                 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+    num = 0
+    while num < 9:
+        ans = str(input("Select letter " + str(num+1) +
+                        " - Type a 'c' for a consonant or a 'v' for a vowel: "))
+        if ans == "c":
+            letters += consonant[random.randint(0, 20)]
+            print("\nLetter " + str(num+1) + " is '" + letters[num] + "'")
+            print("The current string is \"" + letters + "\"\n")
+            num += 1
+        elif ans == "v":
+            letters += vowel[random.randint(0, 4)]
+            print("\nLetter " + str(num+1) + " is '" + letters[num] + "'")
+            print("The current string is \"" + letters + "\"\n")
+            num += 1
+        else:
+            print("\nPlease enter only 'c' or 'v'\n")
+    print("Here are the letters to use \"" + letters + "\"\n")
+    input(" Press enter to exit program")
 
 
 def main():
-    generate_word()
+    """Runs the program"""
+    select_characters()
 
 
 if __name__ == '__main__':
